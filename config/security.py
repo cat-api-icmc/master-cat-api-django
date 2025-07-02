d3d8b14c8094d8b432e0d6a33c0430430fe93f5c
BrunoSilvestre00 <bruno.ap.silvestre@gmail.com>
2025-07-02 11:23:53 -0300

    chore: security

--- config/security.py
+++ config/security.py
@@ -4,7 +4,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 PORT = env('PORT', default=8000, cast=int)
 
-ALLOWED_PORTS = [PORT, 9443, 3000]
+ALLOWED_PORTS = [PORT, 3000, 80, 443, 9443, 8443]
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='*').split(';')
 
@@ -12,9 +12,9 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='*').split(';')
 
 HOSTS = [f'{host}:{port}' for host in ALLOWED_HOSTS for port in ALLOWED_PORTS]
 
-CSRF_TRUSTED_ORIGINS = [f'http://{host}' for host in HOSTS] + [f'https://{host}' for host in HOSTS]
+CSRF_TRUSTED_ORIGINS = [f'{protocol}://{host}' for protocol in ['http', 'https'] for host in HOSTS]
 
-CORS_ORIGIN_WHITELIST = [f'http://{host}' for host in HOSTS] + [f'https://{host}' for host in HOSTS]
+CORS_ORIGIN_WHITELIST = [f'{protocol}://{host}' for protocol in ['http', 'https'] for host in HOSTS]
     
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
