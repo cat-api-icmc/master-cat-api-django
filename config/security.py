ec455391f595fc6911bdd4ab6c20d7614f192e8c
BrunoSilvestre00 <bruno.ap.silvestre@gmail.com>
2025-07-04 03:12:45 -0300

    feat: allow all hosts

--- config/security.py
+++ config/security.py
@@ -2,23 +2,7 @@ from decouple import config as env
 
 SECRET_KEY = env("SECRET_KEY")
 
-PORT = env("PORT", default=8000, cast=int)
-
-ALLOWED_PORTS = [PORT, 3000, 80, 443, 9443, 8443]
-
-ALLOWED_HOSTS = env("ALLOWED_HOSTS", default="*").split(";")
-
-# --------------------------------------------- #
-
-HOSTS = [f"{host}:{port}" for host in ALLOWED_HOSTS for port in ALLOWED_PORTS]
-
-CSRF_TRUSTED_ORIGINS = [
-    f"{protocol}://{host}" for protocol in ["http", "https"] for host in HOSTS
-] + [f"https://{host}" for host in ALLOWED_HOSTS]
-
-CORS_ORIGIN_WHITELIST = [
-    f"{protocol}://{host}" for protocol in ["http", "https"] for host in HOSTS
-] + [f"https://{host}" for host in ALLOWED_HOSTS]
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
