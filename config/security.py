b3c2b35bf265a3fa79530a371d98ec0f701fc146
BrunoSilvestre00 <bruno.ap.silvestre@gmail.com>
2025-07-04 18:03:00 -0300

    feat: CORS_ALLOW_ALL_ORIGINS = True

--- config/security.py
+++ config/security.py
@@ -8,18 +8,10 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://cat.icmc.usp.br",
-    "https://cat.icmc.usp.br:9447",
+    "https://cat.icmc.usp.br:9443",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://cat.icmc.usp.br",
-    "https://cat.icmc.usp.br:9447",
-    "https://cat.icmc.usp.br:8447",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
