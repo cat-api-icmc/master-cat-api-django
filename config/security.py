520bba87be99c7d1ed912c50ec7f73a364848569
BrunoSilvestre00 <bruno.ap.silvestre@gmail.com>
2025-07-04 17:56:52 -0300

    add: more allowed hosts

--- config/security.py
+++ config/security.py
@@ -4,14 +4,22 @@ SECRET_KEY = env("SECRET_KEY")
 
 ALLOWED_HOSTS = ["*"]
 
-ALLOWED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
     "https://cat.icmc.usp.br",
     "https://cat.icmc.usp.br:9447",
 ]
 
-CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS
-
-CORS_ALLOWED_ORIGINS = ALLOWED_ORIGINS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://cat.icmc.usp.br",
+    "https://cat.icmc.usp.br:9447",
+    "https://cat.icmc.usp.br:8447",
+]
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
