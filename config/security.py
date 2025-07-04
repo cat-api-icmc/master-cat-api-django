d2d95aacc25ccd202cb3eb663cc3f572bddb9189
BrunoSilvestre00 <bruno.ap.silvestre@gmail.com>
2025-07-04 17:38:28 -0300

    fix: security

--- config/security.py
+++ config/security.py
@@ -2,7 +2,16 @@ from decouple import config as env
 
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
+
+ALLOWED_ORIGINS = [
+    "https://cat.icmc.usp.br",
+    "https://cat.icmc.usp.br:9447",
+]
+
+CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS
+
+CORS_ALLOWED_ORIGINS = ALLOWED_ORIGINS
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
